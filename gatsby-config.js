695a8c13f185d6d41dda5f72b6a9596adb85068b
Hays <stanfordhays@gmail.com>
2018-06-17 00:20:01 -0500

    Fixed problem with path in gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,13 @@ module.exports = {
   plugins: [
   	'gatsby-plugin-react-helmet',
   	'gatsby-plugin-catch-links',
+  	{
+  		resolve: 'gatsby-source-filesystem',
+  		options: {
+  			path: './src/pages', //Change initial file in path to ${__dirname} if have issues
+  			name: 'pages',
+  		},
+  	},
+  	'gatsby-transformer-remark',
   ],
 }
