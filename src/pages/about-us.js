199da533a9c923adea96ee53361f3d9438f02b2d
Hays <stanfordhays@gmail.com>
2018-07-13 18:29:12 -0500

    Removed unused imports that were in some files

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -1,9 +1,4 @@
 import React from "react";
-import Link from "gatsby-link";
-import Helmet from "react-helmet";
-
-import Welcome from '../components/welcome'
-import Portfolio from '../components/portfolio'
 
 import '../styles/blog-listing.css';
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Link from "gatsby-link";
-import Helmet from "react-helmet";
 
 import Welcome from '../components/welcome'
 import Portfolio from '../components/portfolio'
