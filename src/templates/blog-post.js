b17b6946d9ad925218e55d7149710bca7185d14f
Hays <stanfordhays@gmail.com>
2018-07-18 13:47:42 -0500

    Updated blog post styling

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -5,24 +5,24 @@ export default function Template({data}) {
   const post = data.markdownRemark; 
   return (
     <div className="blog-post-container">
-     <Helmet title={`Leo R. Morales - ${post.frontmatter.title}`} />
+      <Helmet title={`Leo R. Morales - ${post.frontmatter.title}`} />
 
-    <div id="col-wrapper">
-        <div className="column left">
-          <div className="blog-post">
-            <h1>{post.frontmatter.title}</h1>
-            <div
-              className="blog-post-content"
-              dangerouslySetInnerHTML={{ __html: post.html }}
-            />
+      <div id="col-wrapper">
+          <div className="column left">
+            <div id="blog-post" className="blog-post">
+              <h1>{post.frontmatter.title}</h1>
+              <div
+                className="blog-post-content"
+                dangerouslySetInnerHTML={{ __html: post.html }}
+              />
+            </div>
+          </div>
+
+          <div className="column right">
+            <div id="sidebar-section">
+            </div>
           </div>
         </div>
-
-        <div className="column right">
-          <div id="sidebar-section">
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
