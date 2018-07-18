161032db943fe29efb15481fd80f687354388212
Hays <stanfordhays@gmail.com>
2018-07-18 13:59:35 -0500

    Updated links in footer and styled them

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,14 +4,17 @@ import Link from 'gatsby-link'
 const Footer = () => (
   <div id="footer" className="container">
     <div className="title">
-      <h2>Get in touch</h2>
-      <span className="byline">Contact me about anything. I'll get back to you soon.</span> </div>
+      <Link to="/contact/" className="blank-link">
+        <h2>Get in touch</h2>
+        <span className="byline">Contact me about anything. I'll get back to you soon.</span> 
+      </Link>
+    </div>
     <ul className="contact">
-      <li><a href="#" className="icon icon-twitter"><span>Twitter</span></a></li>
-      <li><a href="#" className="icon icon-facebook"><span></span></a></li>
-      <li><a href="#" className="icon icon-dribbble"><span>Pinterest</span></a></li>
-      <li><a href="#" className="icon icon-tumblr"><span>Google+</span></a></li>
-      <li><a href="#" className="icon icon-rss"><span>Pinterest</span></a></li>
+      <li><a href="https://twitter.com/elonmusk" className="icon icon-twitter" target="_blank"><span>Twitter</span></a></li>
+      <li><a href="https://www.facebook.com/ermusk/" className="icon icon-facebook" target="_blank"><span></span></a></li>
+      {/* <li><a href="#" className="icon icon-dribbble" target="_blank"><span>Pinterest</span></a></li> */}
+      {/* <li><a href="#" className="icon icon-tumblr" target="_blank"><span>Google+</span></a></li> */}
+      {/* <li><a href="#" className="icon icon-rss" target="_blank"><span>Pinterest</span></a></li> */}
     </ul>
   </div>
 )
