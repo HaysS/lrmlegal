b17d6476b6701032f8da447e5a75bb265c467398
Hays <stanfordhays@gmail.com>
2018-07-18 13:23:23 -0500

    Added better styling to blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -11,21 +11,27 @@ const Blog = ({data}) => {
   })
 
   return (
-    <div>
-    <div className="blog-posts">
-        {posts
-          .filter(post => post.node.frontmatter.title.length > 0)
-          .map(({ node: post }) => {
-            return (
-              <div className="blog-post-preview" key={post.id}>
-                <h1>
-                  <Link to={post.frontmatter.path}>{post.frontmatter.title}</Link>
-                </h1>
-                <h2>{post.frontmatter.date}</h2>
-                <p>{post.excerpt}</p>
-              </div>
-            );
-          })}
+    <div id="col-wrapper">
+      <div className="column left">
+        <div className="blog-posts">
+          {posts
+            .filter(post => post.node.frontmatter.title.length > 0)
+            .map(({ node: post }) => {
+              return (
+                <div className="blog-post-preview" key={post.id}>
+                  <h1>
+                    <Link to={post.frontmatter.path}>{post.frontmatter.title}</Link>
+                  </h1>
+                  <h2>{post.frontmatter.date}</h2>
+                  <p>{post.excerpt}</p>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+      <div className="column right">
+        <div id="sidebar-section">
+        </div>
       </div>
     </div>
   );
