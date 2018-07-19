223b5256ae03f08ed166930cf6e657aa2a05fe1d
Hays <stanfordhays@gmail.com>
2018-07-19 01:28:49 -0500

    Styled 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  <div id="col-wrapper">
+    <div className="column left">
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </div>
+    <div className="column right">
+    </div>
   </div>
 )
 
