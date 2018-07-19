292de2a80c44cdc8d5ec108bed9e1878bdf22ada
Hays <stanfordhays@gmail.com>
2018-07-19 01:33:53 -0500

    styling 404 page better

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,8 +3,16 @@ import React from 'react'
 const NotFoundPage = () => (
   <div id="col-wrapper">
     <div className="column left">
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <div id="profile">
+        <h1 id="profile-title">
+          404 Error: This Page Does Not Exist.
+        </h1>
+        <div id="profile-body">
+          <p id="profile-text">
+          This page does not exist!
+          </p>
+        </div>
+      </div>
     </div>
     <div className="column right">
     </div>
