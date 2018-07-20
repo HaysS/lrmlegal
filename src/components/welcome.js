37b29194ce66bcfcdddd002d7b922f13dfd2ad12
Hays <stanfordhays@gmail.com>
2018-07-20 14:54:57 -0500

    editted welcome component to not show picture

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -6,17 +6,27 @@ import '../styles/index.css'
 const Welcome = () => (
   <div id="wrapper2">
     <div id="welcome" className="container">
-        <div className="left-col">
+        {/*<div className="left-col">
           <img id="welcome-photo" src={require("../images/lrm-headshot.jpg")} />
-        </div>
-        <div className="right-col">
+        </div>*/}
+        <div className="right-col"style={centerColumn}>
           <div className="title">
-            <h2>Renowned Attorney. Experienced Background.</h2>
+            <h2 style={centerText}>Renowned Attorney. Experienced Background.</h2>
           </div>
-          <p>Leo is a nationally respected trial attorney known for his skill in assessing the legal requirements for business professionals. Leo has helped an incredible amount of businesses navigate their legal needs. Whether you need assistance capturing and finalizing contract terms, reviewing technology service agreements, or just general advice, Leo has your back.</p>
+          <p style={centerText}>Leo is a nationally respected trial attorney known for his skill in assessing the legal requirements for business professionals. Leo has helped an incredible amount of businesses navigate their legal needs. Whether you need assistance capturing and finalizing contract terms, reviewing technology service agreements, or just general advice, Leo has your back.</p>
         </div>
     </div>
   </div>
 )
 
 export default Welcome
+
+const centerText = {
+  textAlign: 'center',
+}
+
+const centerColumn = {
+  float: 'none',
+  padding: '0 0',
+  margin: '0 auto',
+}
