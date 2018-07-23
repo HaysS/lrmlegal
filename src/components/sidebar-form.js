93db38194ed434d67baa06574cbd5146b6d6cbb7
Hays <stanfordhays@gmail.com>
2018-07-22 19:40:43 -0500

    added success page that is now shown after form is submitted

--- src/components/sidebar-form.js
+++ src/components/sidebar-form.js
@@ -89,12 +89,12 @@ class SidebarForm extends React.Component {
         <h1 id="title">{this.props.title}</h1>
         <hr />
         <div id="body">
-          <form name="contact" action="#" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+          <form name="contact" action="/success/" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
             <input type="hidden" name="form-name" value="contact" />
             <input type="hidden" name="bot-field" />
-            <input type="text" name="name" value={this.state.name} placeholder={"Full name"} onChange={this.handleChange} />
+            <input required type="text" name="name" value={this.state.name} placeholder={"Full name"} onChange={this.handleChange} />
             <input type="text" name="phone" value={this.state.phone} placeholder={"Phone number"} onChange={this.handleChange} />
-            <input type="email" name="email" value={this.state.email} placeholder={"Email"} onChange={this.handleChange} />
+            <input required type="email" name="email" value={this.state.email} placeholder={"Email"} onChange={this.handleChange} />
             <textarea type="text" name="message" value={this.state.message} placeholder={"Type your message here..."} onChange={this.handleChange} />
             <input type="submit" value="Submit" />
           </form>
---
+++ src/pages/success.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import Profile from '../components/profile';
+import SidebarForm from '../components/sidebar-form';
+
+import '../styles/blog-listing.css';
+import '../styles/dual-column.css';
+
+const Success = () => {
+  return (
+    <div id="col-wrapper">
+      <div className="column left">
+      	<div id="profile">
+  		    <h1 id="profile-title">Success! Thanks for Reaching Out!</h1>
+  		    <div id="profile-body">
+  		      <p id="profile-text">
+              We've successfully received your message and will get back to you shortly.
+  		        <br /><br />
+  		      </p>
+  		    </div>
+  		  </div>
+      </div>
+      <div className="column right">
+      	<div id="sidebar-section">
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Success
